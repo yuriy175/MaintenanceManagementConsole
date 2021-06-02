efb7c2c6ee8e502f1644d6748c8f04d1230c7518
yuriy_nv@mail.ru <yuriy_nv@mail.ru>
2021-06-02 12:24:12 +0300

    utf -8

--- MessagesSender/MessagesSender/MessagesSender.BL/Helpers/Shapshot/Win32API.cs
+++ MessagesSender/MessagesSender/MessagesSender.BL/Helpers/Shapshot/Win32API.cs
@@ -31,91 +31,91 @@ namespace MessagesSender.MessagesSender.BL.Helpers
         /// </summary>
         internal const int SRCCOPY = 13369376;
 
-		/// <summary>
-		/// enumerates threads
-		/// </summary>
-		/// <param name="hWnd">window handler</param>
-		/// <param name="lParam">lParam</param>
-		/// <returns>result</returns>
-		internal delegate bool EnumThreadDelegate(IntPtr hWnd, IntPtr lParam);
+        /// <summary>
+        /// enumerates threads
+        /// </summary>
+        /// <param name="hWnd">window handler</param>
+        /// <param name="lParam">lParam</param>
+        /// <returns>result</returns>
+        internal delegate bool EnumThreadDelegate(IntPtr hWnd, IntPtr lParam);
 
-		/// <summary>Enumeration of the different ways of showing a window using 
-		/// ShowWindow</summary>
-		internal enum WindowShowStyle : uint
-		{
-			/// <summary>Hides the window and activates another window.</summary>
-			/// <remarks>See SW_HIDE</remarks>
-			Hide = 0,
+        /// <summary>Enumeration of the different ways of showing a window using 
+        /// ShowWindow</summary>
+        internal enum WindowShowStyle : uint
+        {
+            /// <summary>Hides the window and activates another window.</summary>
+            /// <remarks>See SW_HIDE</remarks>
+            Hide = 0,
 
-			/// <summary>Activates and displays a window. If the window is minimized 
-			/// or maximized, the system restores it to its original size and 
-			/// position. An application should specify this flag when displaying 
-			/// the window for the first time.</summary>
-			/// <remarks>See SW_SHOWNORMAL</remarks>
-			ShowNormal = 1,
+            /// <summary>Activates and displays a window. If the window is minimized 
+            /// or maximized, the system restores it to its original size and 
+            /// position. An application should specify this flag when displaying 
+            /// the window for the first time.</summary>
+            /// <remarks>See SW_SHOWNORMAL</remarks>
+            ShowNormal = 1,
 
-			/// <summary>Activates the window and displays it as a minimized window.</summary>
-			/// <remarks>See SW_SHOWMINIMIZED</remarks>
-			ShowMinimized = 2,
+            /// <summary>Activates the window and displays it as a minimized window.</summary>
+            /// <remarks>See SW_SHOWMINIMIZED</remarks>
+            ShowMinimized = 2,
 
-			/// <summary>Activates the window and displays it as a maximized window.</summary>
-			/// <remarks>See SW_SHOWMAXIMIZED</remarks>
-			ShowMaximized = 3,
+            /// <summary>Activates the window and displays it as a maximized window.</summary>
+            /// <remarks>See SW_SHOWMAXIMIZED</remarks>
+            ShowMaximized = 3,
 
-			/// <summary>Maximizes the specified window.</summary>
-			/// <remarks>See SW_MAXIMIZE</remarks>
-			Maximize = 3,
+            /// <summary>Maximizes the specified window.</summary>
+            /// <remarks>See SW_MAXIMIZE</remarks>
+            Maximize = 3,
 
-			/// <summary>Displays a window in its most recent size and position. 
-			/// This value is similar to "ShowNormal", except the window is not 
-			/// actived.</summary>
-			/// <remarks>See SW_SHOWNOACTIVATE</remarks>
-			ShowNormalNoActivate = 4,
+            /// <summary>Displays a window in its most recent size and position. 
+            /// This value is similar to "ShowNormal", except the window is not 
+            /// actived.</summary>
+            /// <remarks>See SW_SHOWNOACTIVATE</remarks>
+            ShowNormalNoActivate = 4,
 
-			/// <summary>Activates the window and displays it in its current size 
-			/// and position.</summary>
-			/// <remarks>See SW_SHOW</remarks>
-			Show = 5,
+            /// <summary>Activates the window and displays it in its current size 
+            /// and position.</summary>
+            /// <remarks>See SW_SHOW</remarks>
+            Show = 5,
 
-			/// <summary>Minimizes the specified window and activates the next 
-			/// top-level window in the Z order.</summary>
-			/// <remarks>See SW_MINIMIZE</remarks>
-			Minimize = 6,
+            /// <summary>Minimizes the specified window and activates the next 
+            /// top-level window in the Z order.</summary>
+            /// <remarks>See SW_MINIMIZE</remarks>
+            Minimize = 6,
 
-			/// <summary>Displays the window as a minimized window. This value is 
-			/// similar to "ShowMinimized", except the window is not activated.</summary>
-			/// <remarks>See SW_SHOWMINNOACTIVE</remarks>
-			ShowMinNoActivate = 7,
+            /// <summary>Displays the window as a minimized window. This value is 
+            /// similar to "ShowMinimized", except the window is not activated.</summary>
+            /// <remarks>See SW_SHOWMINNOACTIVE</remarks>
+            ShowMinNoActivate = 7,
 
-			/// <summary>Displays the window in its current size and position. This 
-			/// value is similar to "Show", except the window is not activated.</summary>
-			/// <remarks>See SW_SHOWNA</remarks>
-			ShowNoActivate = 8,
+            /// <summary>Displays the window in its current size and position. This 
+            /// value is similar to "Show", except the window is not activated.</summary>
+            /// <remarks>See SW_SHOWNA</remarks>
+            ShowNoActivate = 8,
 
-			/// <summary>Activates and displays the window. If the window is 
-			/// minimized or maximized, the system restores it to its original size 
-			/// and position. An application should specify this flag when restoring 
-			/// a minimized window.</summary>
-			/// <remarks>See SW_RESTORE</remarks>
-			Restore = 9,
+            /// <summary>Activates and displays the window. If the window is 
+            /// minimized or maximized, the system restores it to its original size 
+            /// and position. An application should specify this flag when restoring 
+            /// a minimized window.</summary>
+            /// <remarks>See SW_RESTORE</remarks>
+            Restore = 9,
 
-			/// <summary>Sets the show state based on the SW_ value specified in the 
-			/// STARTUPINFO structure passed to the CreateProcess function by the 
-			/// program that started the application.</summary>
-			/// <remarks>See SW_SHOWDEFAULT</remarks>
-			ShowDefault = 10,
+            /// <summary>Sets the show state based on the SW_ value specified in the 
+            /// STARTUPINFO structure passed to the CreateProcess function by the 
+            /// program that started the application.</summary>
+            /// <remarks>See SW_SHOWDEFAULT</remarks>
+            ShowDefault = 10,
 
-			/// <summary>Windows 2000/XP: Minimizes a window, even if the thread 
-			/// that owns the window is hung. This flag should only be used when 
-			/// minimizing windows from a different thread.</summary>
-			/// <remarks>See SW_FORCEMINIMIZE</remarks>
-			ForceMinimized = 11
-		}
+            /// <summary>Windows 2000/XP: Minimizes a window, even if the thread 
+            /// that owns the window is hung. This flag should only be used when 
+            /// minimizing windows from a different thread.</summary>
+            /// <remarks>See SW_FORCEMINIMIZE</remarks>
+            ForceMinimized = 11
+        }
 
-		/// <summary>
-		/// rectangle struct
-		/// </summary>
-		[StructLayout(LayoutKind.Sequential)]
+        /// <summary>
+        /// rectangle struct
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
         internal struct RECT
         {
             /// <summary>
@@ -166,46 +166,67 @@ namespace MessagesSender.MessagesSender.BL.Helpers
             }
         }
 
-		/// <summary>
-		/// Get window class name
-		/// </summary>
-		/// <param name="hWnd">window handler</param>
-		/// <param name="param">param</param>
-		/// <param name="length">length</param>
-		[DllImport("User32.Dll")]
+        /// <summary>
+        /// Get window class name
+        /// </summary>
+        /// <param name="hWnd">window handler</param>
+        /// <param name="param">param</param>
+        /// <param name="length">length</param>
+        [DllImport("User32.Dll")]
         internal static extern void GetClassName(IntPtr hWnd, System.Text.StringBuilder param, int length);
 
-		/// <summary>
-		/// Get window text length
-		/// </summary>
-		/// <param name="hWnd">window handler</param>
-		/// <returns>length</returns>
-		[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        /// <summary>
+        /// Get window text length
+        /// </summary>
+        /// <param name="hWnd">window handler</param>
+        /// <returns>length</returns>
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         internal static extern int GetWindowTextLength(IntPtr hWnd);
 
-		/// <summary>
-		/// Get window text
-		/// </summary>
-		/// <param name="hWnd">window handler</param>
-		/// <param name="lpString">lpString</param>
-		/// <param name="nMaxCount">nMaxCount</param>
-		/// <returns>length</returns>
-		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        /// <summary>
+        /// Get window text
+        /// </summary>
+        /// <param name="hWnd">window handler</param>
+        /// <param name="lpString">lpString</param>
+        /// <param name="nMaxCount">nMaxCount</param>
+        /// <returns>length</returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         internal static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
 
-        [DllImport("user32.dll")]
+		/// <summary>
+		/// Enumerates thread windows
+		/// </summary>
+		/// <param name="dwThreadId">thread id</param>
+		/// <param name="lpfn">lpfn</param>
+		/// <param name="lParam">lParam</param>
+		/// <returns>result</returns>
+		[DllImport("user32.dll")]
         internal static extern bool EnumThreadWindows(uint dwThreadId, Win32API.EnumThreadDelegate lpfn, IntPtr lParam);
 
-        [DllImport("user32.dll")]
+		/// <summary>
+		/// Get system menu
+		/// </summary>
+		/// <param name="hWnd">window handler</param>
+		/// <param name="bRevert">bRevert</param>
+		/// <returns>handler</returns>
+		[DllImport("user32.dll")]
         internal static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
 
-        [DllImport("user32.dll")]
-        internal static extern bool AppendMenu(IntPtr hMenu, int wFlags, int wIDNewItem, string lpNewItem);
-
-        [DllImport("user32.dll")]
+		/// <summary>
+		/// Show window
+		/// </summary>
+		/// <param name="hWnd">window handler</param>
+		/// <param name="nCmdShow">show style</param>
+		/// <returns>result</returns>
+		[DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static extern bool ShowWindow(IntPtr hWnd, WindowShowStyle nCmdShow);
 
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="lprc"></param>
+		/// <returns>result</returns>
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         internal static extern bool IsRectEmpty([In] ref RECT lprc);
